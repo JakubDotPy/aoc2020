4c611b321a92c61d607249026972385475f3db73
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2020-12-01 21:42:25 +0100

    day01

---
+++ day01/part1.py
@@ -0,0 +1,42 @@
+import argparse
+
+import pytest
+
+from support.support import timing
+
+
+def compute(s: str) -> int:
+    nums = set(map(int, s.splitlines()))
+    for num in nums:
+        if 2020 - num in nums:
+            return num * (2020 - num)
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        ("""1721
+979
+366
+299
+675
+1456""", 514579),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file')
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ day01/part2.py
@@ -0,0 +1,45 @@
+import argparse
+from itertools import tee
+
+import pytest
+
+from support.support import timing
+
+
+def compute(s: str) -> int:
+    nums = sorted(set(map(int, s.splitlines())))
+    a, b = tee(nums)
+    next(b, None)
+    for prev, nex in zip(a, b):
+        search = 2020 - (prev + nex)
+        if search in nums:
+            return search * prev * nex
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            ("""979
+            366
+            675
+            299
+            1456""", 241861950),
+            ),
+    )
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file')
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
