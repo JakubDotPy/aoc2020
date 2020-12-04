b764fc9434dcbdcbe1c3312fea616e8a5b6b3105
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2020-12-04 09:25:48 +0100

    day04 part1

---
+++ day04/part1.py
@@ -0,0 +1,68 @@
+import argparse
+
+import pytest
+
+from support.support import timing
+
+"""
+The automatic passport scanners are slow because they're having trouble detecting
+which passports have all required fields. The expected fields are as follows:
+
+byr (Birth Year)
+iyr (Issue Year)
+eyr (Expiration Year)
+hgt (Height)
+hcl (Hair Color)
+ecl (Eye Color)
+pid (Passport ID)
+cid (Country ID)   - optional
+
+"""
+
+
+def compute(s: str) -> int:
+    passports = [dict(param.split(':') for param in passport.split()) for passport in s.split('\n\n')]
+    required = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', }  # 'cid' Country ID is optional for now
+    return sum(required.issubset(set(passport.keys())) for passport in passports)
+
+
+INPUT_S = """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
+"""
+
+
+@pytest.mark.solved
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, 2),
+            ),
+    )
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file')
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
