70eeb737ec5b34140f5ef915614c94ba4786e0e2
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2020-12-17 09:28:04 +0100

    day17 - part1 - refactored to cubes in 3D

--- day17/part1.py
+++ day17/part1.py
@@ -1,5 +1,6 @@
 import argparse
 import os.path
+from itertools import count, product
 
 import pytest
 
@@ -27,59 +28,78 @@ def analyze_grid():
     active_cubes = set()
     inactive_cubes = set()
 
-    for space, value in GRID.items():
+    for cube, value in GRID.items():
         if value == '#':
-            active_cubes.add(space)
+            active_cubes.add(cube)
         elif value == '.':
-            inactive_cubes.add(space)
-        else:  # floor
-            continue
+            inactive_cubes.add(cube)
 
 
-def active_around(space) -> int:
-    """counts active cubes around (8 ways) selected"""
-    x, y = space
-
-    return ((x - 1, y - 1) in active_cubes) \
-           + ((x, y - 1) in active_cubes) \
-           + ((x + 1, y - 1) in active_cubes) \
-           + ((x - 1, y) in active_cubes) \
-           + ((x + 1, y) in active_cubes) \
-           + ((x - 1, y + 1) in active_cubes) \
-           + ((x, y + 1) in active_cubes) \
-           + ((x + 1, y + 1) in active_cubes)
+cubes_around = set(product((-1, 0, 1), repeat=3)) - {(0, 0, 0)}
 
 
-def apply_rules(space):
+def active_around(cube) -> int:
+    """counts active cubes around (3D) selected"""
+    x, y, z = cube
+
+    around = 0
+    for s_x, s_y, s_z in cubes_around:
+        check = x + s_x, y + s_y, z + s_z
+        if check in active_cubes:
+            around += 1
+
+    # TODO: return sum((x + mov[0], y + mov[1]) in active_cubes for mov in cubes_around)
+    return around
+
+
+def apply_rules(cube):
+    """Applies rules to cube to determine if it will be active or not.
+
+    If a cube is active and exactly 2 or 3 of its neighbors are also active,
+     the cube remains active. Otherwise, the cube becomes inactive.
+
+    If a cube is inactive but exactly 3 of its neighbors are active,
+     the cube becomes active. Otherwise, the cube remains inactive.
+    """
     global to_activate, to_deactivate
 
-    if space in inactive_cubes and active_around(space) == 0:
-        to_activate.add(space)
-    if space in active_cubes and active_around(space) >= 4:
-        to_deactivate.add(space)
+    if cube in inactive_cubes and active_around(cube) in (2, 3):
+        to_activate.add(cube)
+    else:
+        to_deactivate.add(cube)
+
+    if cube in inactive_cubes and active_around(cube) == 3:
+        to_activate.add(cube)
+    else:
+        to_deactivate.add(cube)
 
 
 def apply_cubes():
-    for space in to_activate:
-        GRID[space] = '#'
-    for space in to_deactivate:
-        GRID[space] = 'L'
+    for cube in to_activate:
+        GRID[cube] = '#'
+    for cube in to_deactivate:
+        GRID[cube] = '.'
 
 
 def pprint_grid(width, length):
     global GRID
-    for y in range(length + 1):
-        for x in range(width + 1):
-            print(GRID[(x, y)], end='')
-        print()
-    print('-' * 50)
+
+    layers = sorted(list(set(cube[2] for cube in GRID)))
+
+    for z in layers:
+        print(f' layer {z} '.center(20, '-'))
+        for y in range(length + 1):
+            for x in range(width + 1):
+                print(GRID[(x, y, z)], end='')
+            print()
+    print('=' * 20)
 
 
 def load_grid(s):
     global GRID
     for y, line in enumerate(s.splitlines()):
         for x, char in enumerate(line):
-            GRID[(x, y)] = char
+            GRID[(x, y, 0)] = char  # z coordinate is 0 in the beginning
     return x, y
 
 
@@ -87,20 +107,26 @@ def compute(s: str) -> int:
     global to_activate, to_deactivate
     grid_width, grid_length = load_grid(s)
 
-    while True:
-        # pprint_grid(grid_width, grid_length)
+    # THE LOOP
+    for cycle in count(start=1):
+        print(f' cycle {cycle} '.center(30, '#'))
+        pprint_grid(grid_width, grid_length)
 
         analyze_grid()
 
         to_activate = set()
         to_deactivate = set()
 
-        for space in GRID:
-            apply_rules(space)
+        for cube in GRID:
+            apply_rules(cube)
+
+        assert len(to_activate) + len(to_deactivate) == len(GRID)
 
         if to_activate or to_deactivate:
             apply_cubes()
-        else:
+
+        # stop condition
+        if cycle == 3:
             return len(active_cubes)
 
 
